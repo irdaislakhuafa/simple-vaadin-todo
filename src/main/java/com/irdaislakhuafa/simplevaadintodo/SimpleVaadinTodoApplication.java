ef6eea1af988de086b9c1aff248467da7d3ace33
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-13 08:56:26 +0700

    first commit

---
+++ src/main/java/com/irdaislakhuafa/simplevaadintodo/SimpleVaadinTodoApplication.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.simplevaadintodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVaadinTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleVaadinTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irdaislakhuafa/simplevaadintodo/SimpleVaadinTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.simplevaadintodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleVaadinTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
