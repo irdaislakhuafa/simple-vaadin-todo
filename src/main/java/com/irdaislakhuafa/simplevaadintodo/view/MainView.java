152398ccea6459e6a3eeec75a0d79a8a4657d7dc
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-10-21 01:50:17 +0700

    add page title

--- src/main/java/com/irdaislakhuafa/simplevaadintodo/view/MainView.java
+++ src/main/java/com/irdaislakhuafa/simplevaadintodo/view/MainView.java
@@ -3,9 +3,11 @@ package com.irdaislakhuafa.simplevaadintodo.view;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "/")
+@PageTitle(value = "Simple Vaadin Todo")
 @CssImport(value = "./style.css")
 public class MainView extends VerticalLayout {
     public MainView() {
