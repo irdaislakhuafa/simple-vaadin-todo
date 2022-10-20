81375bf4d2e136a91ff6bd87d90d88aea236c1f4
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-10-21 02:30:46 +0700

    add label, input text field and add button

--- src/main/java/com/irdaislakhuafa/simplevaadintodo/view/MainView.java
+++ src/main/java/com/irdaislakhuafa/simplevaadintodo/view/MainView.java
@@ -1,8 +1,18 @@
 package com.irdaislakhuafa.simplevaadintodo.view;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.Autocapitalize;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -10,9 +20,48 @@ import com.vaadin.flow.router.Route;
 @PageTitle(value = "Simple Vaadin Todo")
 @CssImport(value = "./style.css")
 public class MainView extends VerticalLayout {
+    private final int notifDuration = (1000 * 5);
+    private final Position notifPosition = Position.BOTTOM_CENTER;
+
     public MainView() {
-        var helloWorld = new H1("Hello World");
-        helloWorld.addClassName("center");
-        add(helloWorld);
+        final var labelApp = new H1("Simple Vaadin Todo");
+        labelApp.addClassName("center");
+        add(labelApp);
+
+        // to store todo list
+        final var todoList = new VerticalLayout();
+
+        // input text
+        var inputText = new TextField();
+        inputText.setPlaceholder("Input Text Here!");
+        inputText.setAutocapitalize(Autocapitalize.WORDS);
+        inputText.addValueChangeListener((v) -> {
+            if (v.getValue().contains("babi")) {
+                Notification.show(String.format("Word \"%s\" not allowed", v.getValue()), notifDuration, notifPosition);
+            }
+        });
+
+        // add button for todo list
+        final var addButton = new Button(new Icon(VaadinIcon.PLUS));
+        addButton.addClickListener((button) -> {
+            final var text = inputText.getValue();
+            if (text.isBlank() || text.isBlank()) {
+                Notification.show("Please insert text first!", notifDuration, notifPosition);
+                return;
+            }
+
+            final var checkBox = new Checkbox(text);
+            todoList.add(checkBox);
+        });
+
+        final var inputTodoList = new HorizontalLayout(inputText, addButton);
+        inputTodoList.addClassName("center");
+        add(inputTodoList);
+
+        add(new HorizontalLayout(todoList) {
+            {
+                addClassName("checkbox-view");
+            }
+        });
     }
 }
